281e626c8a2da456b4a48bc37856ccf083edc5bf
Hugo López Fernández <hlfernandez@uvigo.es>
2017-01-19 12:19:41 +0000

    Add support to batch transcript analysis with StringTie

---
+++ rnaseq-app-aibench/src/main/java/org/sing_group/rnaseq/aibench/operations/batch/StringTieBatch.java
@@ -0,0 +1,54 @@
+package org.sing_group.rnaseq.aibench.operations.batch;
+
+import java.io.File;
+import java.util.Arrays;
+
+import es.uvigo.ei.aibench.core.Core;
+import es.uvigo.ei.aibench.core.operation.annotation.Direction;
+import es.uvigo.ei.aibench.core.operation.annotation.Operation;
+import es.uvigo.ei.aibench.core.operation.annotation.Port;
+
+@Operation(
+	name = "Reconstruct transcripts using StringTie", 
+	description = "Reconstructs transcripts using StringTie."
+)
+public class StringTieBatch {
+	private File referenceAnnotationFile;
+	private File[] inputBamFiles;
+
+	@Port(
+		direction = Direction.INPUT, 
+		name = "Reference annotation file",
+		description = "Reference annotation file (.gtf)",
+		allowNull = false,
+		order = 1,
+		extras="selectionMode=files"
+	)
+	public void setReferenceAnnotationFile(File referenceAnnotationFile) {
+		this.referenceAnnotationFile = referenceAnnotationFile;
+	}
+	
+	@Port(
+		direction = Direction.INPUT, 
+		name = "Input bam files",
+		description = "Input bam files.",
+		allowNull = false,
+		order = 2,
+		extras="selectionMode=files"
+	)
+	public void setInputBamFile(File[] inputBamFiles) {
+		this.inputBamFiles = inputBamFiles;
+
+		this.runOperation();
+	}
+
+	private void runOperation() {
+		for(final File f : inputBamFiles) {
+			Core.getInstance().executeOperation(
+				"operations.stringtie", null, 
+				Arrays.asList(new Object[]{this.referenceAnnotationFile, f, null})
+			);
+		}
+	}
+}
+
