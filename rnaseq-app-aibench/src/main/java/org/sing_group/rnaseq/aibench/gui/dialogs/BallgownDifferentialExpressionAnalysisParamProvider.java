86b9ce1f19ec27b8593abc66d5ce61ddcbf97f98
Hugo López Fernández <hlfernandez@uvigo.es>
2017-02-09 12:04:53 +0000

    Add an opration to run ballgown differential expression analysis

---
+++ rnaseq-app-aibench/src/main/java/org/sing_group/rnaseq/aibench/gui/dialogs/BallgownDifferentialExpressionAnalysisParamProvider.java
@@ -0,0 +1,60 @@
+package org.sing_group.rnaseq.aibench.gui.dialogs;
+
+import static es.uvigo.ei.aibench.core.CoreUtils.createParams;
+import static java.util.Arrays.asList;
+
+import javax.swing.JComponent;
+import javax.swing.event.ChangeEvent;
+
+import org.sing_group.rnaseq.gui.ballgown.BallgownSamplesEditor;
+import org.sing_group.rnaseq.gui.ballgown.listener.BallgownSamplesEditorListener;
+
+import es.uvigo.ei.aibench.core.ParamSpec;
+import es.uvigo.ei.aibench.workbench.inputgui.AbstractParamProvider;
+
+public class BallgownDifferentialExpressionAnalysisParamProvider
+	extends AbstractParamProvider 
+	implements BallgownSamplesEditorListener 
+{
+	private BallgownSamplesEditor ballgownSamplesEditor;
+
+	@Override
+	public JComponent getComponent() {
+		if (this.ballgownSamplesEditor == null) {
+			this.ballgownSamplesEditor = new BallgownSamplesEditor();
+			this.ballgownSamplesEditor.addBallgownSamplesEditorListener(this);
+		}
+		return this.ballgownSamplesEditor;
+	}
+
+	@Override
+	public ParamSpec getParamSpec() throws IllegalArgumentException {
+		return createParams(asList(this.ballgownSamplesEditor.getSamples()))[0];
+	}
+
+	@Override
+	public boolean isValidValue() {
+		return 	this.ballgownSamplesEditor.isValidSelection() && 
+				!this.ballgownSamplesEditor.getSamples().isEmpty();
+	}
+
+	@Override
+	public void onSampleEdited(ChangeEvent event) {
+		onBallgownSamplesEditorEvent();
+	}
+
+	@Override
+	public void onSampleAdded(ChangeEvent event) {
+		onBallgownSamplesEditorEvent();
+	}
+
+	@Override
+	public void onSampleRemoved(ChangeEvent event) {
+		onBallgownSamplesEditorEvent();
+	}
+
+	private void onBallgownSamplesEditorEvent() {
+		this.setChanged();
+		this.notifyObservers();
+	}
+}
---
+++ rnaseq-app-aibench/src/main/java/org/sing_group/rnaseq/aibench/operations/BallgownDifferentialExpressionAnalysis.java
@@ -0,0 +1,68 @@
+package org.sing_group.rnaseq.aibench.operations;
+
+import static javax.swing.SwingUtilities.invokeLater;
+
+import java.io.File;
+
+import org.sing_group.rnaseq.aibench.gui.dialogs.BallgownDifferentialExpressionAnalysisParamsWindow;
+import org.sing_group.rnaseq.api.entities.ballgown.BallgownSamples;
+import org.sing_group.rnaseq.api.environment.execution.ExecutionException;
+import org.sing_group.rnaseq.core.controller.DefaultAppController;
+
+import es.uvigo.ei.aibench.core.operation.annotation.Direction;
+import es.uvigo.ei.aibench.core.operation.annotation.Operation;
+import es.uvigo.ei.aibench.core.operation.annotation.Port;
+import es.uvigo.ei.aibench.workbench.Workbench;
+
+@Operation(
+	name = "Differential expression analysis with Ballgown", 
+	description = "Performs a differential expression analysis using Ballgown."
+)
+public class BallgownDifferentialExpressionAnalysis {
+	private BallgownSamples samples;
+	private File directory;
+
+	@Port(
+		direction = Direction.INPUT, 
+		name = BallgownDifferentialExpressionAnalysisParamsWindow.SAMPLES,
+		description = "List of samples to analyze",
+		allowNull = false,
+		order = 1
+	)
+	public void setSamples(BallgownSamples samples) {
+		this.samples = samples;
+	}
+
+	@Port(
+		direction = Direction.INPUT, 
+		name = "Directory",
+		description = "Directory to store analysis results",
+		allowNull = false,
+		order = 2,
+		extras="selectionMode=directories"
+	)
+	public void setDirectory(File directory) {
+		this.directory = directory;
+
+		this.runOperation();
+	}
+
+	private void runOperation() {
+		try {
+			DefaultAppController.getInstance().getBallgownController()
+				.differentialExpression(samples, this.directory);
+			invokeLater(this::succeed);
+		} catch (ExecutionException e) {
+			Workbench.getInstance().error(e, e.getMessage());
+		} catch (InterruptedException e) {
+			Workbench.getInstance().error(e, e.getMessage());
+		}
+	}
+	
+	private void succeed() {
+		Workbench.getInstance().info(
+			"Succeed: ballgown analysis results can be found at " + 
+			this.directory.getAbsolutePath() + "."
+		);
+	}
+}
