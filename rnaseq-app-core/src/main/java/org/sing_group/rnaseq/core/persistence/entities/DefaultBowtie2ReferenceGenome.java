ce27dfef2edefadb947bebde1e5d43bb92c1f859
Hugo López Fernández <hlfernandez@uvigo.es>
2017-03-02 18:44:40 +0000

    Fix a bug when looking for the indexes
    Fix a bug when looking for the indexes that was causing that index
    prefix could be incorrectly extracted.

--- rnaseq-app-core/src/main/java/org/sing_group/rnaseq/core/persistence/entities/DefaultBowtie2ReferenceGenome.java
+++ rnaseq-app-core/src/main/java/org/sing_group/rnaseq/core/persistence/entities/DefaultBowtie2ReferenceGenome.java
@@ -9,12 +9,14 @@ import org.sing_group.rnaseq.api.persistence.entities.Bowtie2ReferenceGenome;
 
 public class DefaultBowtie2ReferenceGenome implements Bowtie2ReferenceGenome {
 	private static final long serialVersionUID = 1L;
-	private static final String[] INDEXES = { ".1.bt2", ".2.bt2", ".3.bt2",
-		".4.bt2", ".rev.1.bt2", ".rev.2.bt2" };
+	private static final String[] INDEXES = { 
+		".1.bt2", ".2.bt2", 	".3.bt2",
+		".4.bt2", ".rev.1.bt2", ".rev.2.bt2" 
+	};
 
 	private File referenceGenome;
 	private String referenceGenomeIndex;
-	
+
 	public DefaultBowtie2ReferenceGenome(File file, String index) {
 		this.referenceGenome = file;
 		this.referenceGenomeIndex = index;
@@ -26,10 +28,15 @@ public class DefaultBowtie2ReferenceGenome implements Bowtie2ReferenceGenome {
 	}
 
 	private String lookForIndex(File indexFolder) {
-		if(indexFolder.isDirectory()) {
-			Optional<File> firstIndexFile = asList(indexFolder.listFiles()).stream().filter(f -> f.getName().endsWith(INDEXES[0])).findAny();
-			if(firstIndexFile.isPresent()) {
-				return firstIndexFile.get().getAbsolutePath().replace(INDEXES[0], "");
+		if (indexFolder.isDirectory()) {
+			Optional<File> firstIndexFile = asList(indexFolder.listFiles())
+				.stream().filter(f -> f.getName().endsWith(INDEXES[5]))
+				.findAny();
+
+			if (firstIndexFile.isPresent()) {
+				String absolutePath = firstIndexFile.get().getAbsolutePath();
+				return 	absolutePath.substring(
+							0, absolutePath.indexOf(INDEXES[5]));
 			}
 			throw new IllegalArgumentException("indexFolder must contain bowtie2 indexes");
 		}
@@ -58,15 +65,15 @@ public class DefaultBowtie2ReferenceGenome implements Bowtie2ReferenceGenome {
 	}
 
 	private boolean isValidIndex() {
-		if(!getReferenceGenomeIndex().isPresent()) {
+		if (!getReferenceGenomeIndex().isPresent()) {
 			return false;
 		}
+
 		String index = getReferenceGenomeIndex().get();
 		boolean valid = true;
-		for(String extension : INDEXES) {
+		for (String extension : INDEXES) {
 			valid = valid && new File(index + extension).exists();
 		}
 		return valid;
 	}
-
 }
