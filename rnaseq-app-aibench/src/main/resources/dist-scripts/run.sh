351babc400110e14ea2d860fea5a212f073b7a00
Hugo López Fernández <hlfernandez@uvigo.es>
2017-07-17 11:39:08 +0100

    Fix run.sh distribution script

--- rnaseq-app-aibench/src/main/resources/dist-scripts/run.sh
+++ rnaseq-app-aibench/src/main/resources/dist-scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java/bin/java -Xmx4G -Dswing.defaultlaf=javax.swing.plaf.nimbus.NimbusLookAndFeel -jar ./lib/aibench-aibench-${aibench.version}.jar
+java -Xmx4G -Dswing.defaultlaf=javax.swing.plaf.nimbus.NimbusLookAndFeel -jar ./lib/aibench-aibench-${aibench.version}.jar
