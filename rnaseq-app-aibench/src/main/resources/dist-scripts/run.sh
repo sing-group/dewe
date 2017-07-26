83d833da4fbece71f07720bd29dfef32bb80dbcc
Hugo López Fernández <hlfernandez@uvigo.es>
2017-07-26 12:44:34 +0100

    Improve run scripts

--- rnaseq-app-aibench/src/main/resources/dist-scripts/run.sh
+++ rnaseq-app-aibench/src/main/resources/dist-scripts/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+
+NPROC='threads='$(nproc)
+sed -i "s/threads=.*/${NPROC}/" conf/app.conf
 
 java -Xmx4G -Dswing.defaultlaf=javax.swing.plaf.nimbus.NimbusLookAndFeel -jar ./lib/aibench-aibench-${aibench.version}.jar
