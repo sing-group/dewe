2c214799924775b2b54901552cb7c332b6678e6c
mahmi <abmiguez@esei.uvigo.es>
2018-06-22 14:01:36 +0200

    Adds image size selection in Heatmap

--- rnaseq-app-core/src/main/resources/scripts/ballgown/ballgown-differential-expression.R
+++ rnaseq-app-core/src/main/resources/scripts/ballgown/ballgown-differential-expression.R
@@ -11,6 +11,10 @@ library(ballgown)
 library(genefilter)
 library(dplyr)
 library(devtools)
+library(calibrate)
+library(pheatmap)
+library(ggplot2)
+library(ggrepel)
 
 getFpkmPlus1 <- function(ballgownData) {
 	fpkm <- texpr(bg, meas="FPKM")
@@ -155,15 +159,15 @@ dev.off()
 consensuspathdb <- cbind(results_genes[6], log2(results_genes[3]))
 consensuspathdb <- consensuspathdb[consensuspathdb[,1]!=".",]
 
-write.table(consensuspathdb, col.names=FALSE, row.names = FALSE, paste(workingDirectory, "consensuspathdb_enrichment_analysis.csv" ,sep=""), sep="\t")
+write.table(consensuspathdb, col.names=FALSE, row.names = FALSE, paste(workingDirectory, "consensuspathdb-enrichment-analysis.csv" ,sep=""), sep="\t")
 
-##Volcano plot 
+## Volcano plot 
 if(image.format == "jpeg") {
-	jpeg(paste(workingDirectory, 'volcanoplot.jpeg',sep=""), width = image.width, height = image.height)
+	jpeg(paste(workingDirectory, 'volcano-plot.jpeg',sep=""), width = image.width, height = image.height)
 } else if(image.format == "tiff") {
-	tiff(paste(workingDirectory, 'volcanoplot.tiff',sep=""), width = image.width, height = image.height)
+	tiff(paste(workingDirectory, 'volcano-plot.tiff',sep=""), width = image.width, height = image.height)
 } else if(image.format == "png") {
-	png(paste(workingDirectory, 'volcanoplot.png',sep=""), width = image.width, height = image.height)
+	png(paste(workingDirectory, 'volcano-plot.png',sep=""), width = image.width, height = image.height)
 }
 
 colors=c(gray.colors(4, start = 0.8, end = 0.2, gamma = 2.2, alpha = NULL))
@@ -190,7 +194,6 @@ points(fc_sig_results_genes_plot$logfc,-log10(fc_sig_results_genes_plot$pval), c
 points(qval_fc_sig_results_genes_plot$logfc,-log10(qval_fc_sig_results_genes_plot$pval), col=colors[4], pch=20) 
 
 # label the significant genes
-require(calibrate)
 textxy(qval_fc_sig_results_genes_plot$logfc,-log10(qval_fc_sig_results_genes_plot$pval), labs=qval_fc_sig_results_genes_plot$id, cex=.6)
 
 legend("bottomright", xjust=1, yjust=1, legend=c("FDR<0.05", "|log2(fold change)|>1", "both"), pch=20, col=c(colors[2],colors[3],colors[4]))
@@ -199,13 +202,13 @@ dev.off()
 
 conditions <- unique(pheno_data$type)
 
-##Histogram distribution of differential expression values
+## Histogram distribution of differential expression values
 if(image.format == "jpeg") {
-	jpeg(paste(workingDirectory, 'distr_de_values.jpeg',sep=""), width = image.width, height = image.height)
+	jpeg(paste(workingDirectory, 'DE-values-distribution.jpeg',sep=""), width = image.width, height = image.height)
 } else if(image.format == "tiff") {
-	tiff(paste(workingDirectory, 'distr_de_values.tiff',sep=""), width = image.width, height = image.height)
+	tiff(paste(workingDirectory, 'DE-values-distribution.tiff',sep=""), width = image.width, height = image.height)
 } else if(image.format == "png") {
-	png(paste(workingDirectory, 'distr_de_values.png',sep=""), width = image.width, height = image.height)
+	png(paste(workingDirectory, 'DE-values-distribution.png',sep=""), width = image.width, height = image.height)
 }
 
 # Identify the genes (rows) with adjusted p-value (i.eq-value) < 0.05 
@@ -224,11 +227,11 @@ dev.off()
 
 ## Correlation plot between treatment/disease and control samples. 
 if(image.format == "jpeg") {
-	jpeg(paste(workingDirectory, 'correlation_plot.jpeg',sep=""), width = image.width, height = image.height)
+	jpeg(paste(workingDirectory, 'FPKM-conditions-correlation.jpeg',sep=""), width = image.width, height = image.height)
 } else if(image.format == "tiff") {
-	tiff(paste(workingDirectory, 'correlation_plot.tiff',sep=""), width = image.width, height = image.height)
+	tiff(paste(workingDirectory, 'FPKM-conditions-correlation.tiff',sep=""), width = image.width, height = image.height)
 } else if(image.format == "png") {
-	png(paste(workingDirectory, 'correlation_plot.png',sep=""), width = image.width, height = image.height)
+	png(paste(workingDirectory, 'FPKM-conditions-correlation.png',sep=""), width = image.width, height = image.height)
 }
 
 colors=c('gray65','gray40','gray10')
@@ -265,13 +268,13 @@ textxy(xfsig,yfsig, cex=.6, labs=row.names(gene_expression[fsig,]))
 
 dev.off()
 
-##Density plot
+## Density plot
 if(image.format == "jpeg") {
-	jpeg(paste(workingDirectory, 'density_plot.jpeg',sep=""), width = image.width, height = image.height)
+	jpeg(paste(workingDirectory, 'FPKM-conditions-density-plot.jpeg',sep=""), width = image.width, height = image.height)
 } else if(image.format == "tiff") {
-	tiff(paste(workingDirectory, 'density_plot.tiff',sep=""), width = image.width, height = image.height)
+	tiff(paste(workingDirectory, 'FPKM-conditions-density-plot.tiff',sep=""), width = image.width, height = image.height)
 } else if(image.format == "png") {
-	png(paste(workingDirectory, 'density_plot.png',sep=""), width = image.width, height = image.height)
+	png(paste(workingDirectory, 'FPKM-conditions-density-plot.png',sep=""), width = image.width, height = image.height)
 }
 colors = colorRampPalette(gray.colors(5, start = 1.0, end = 0.3, gamma = 2.2, alpha = NULL))
 if(image.color){
@@ -280,12 +283,13 @@ if(image.color){
 par(mfrow=c(1,2))
 plot(x,y, xlab=paste(conditions[1],"log2(FPKM+1)",sep=" "), ylab=paste(conditions[2],"log2(FPKM+1)",sep=" "))
 smoothScatter(x,y, colramp = colors, xlab=paste(conditions[1],"log2(FPKM+1)",sep=" "), ylab=paste(conditions[2],"log2(FPKM+1)",sep=" "))
+title(main=paste(conditions[2], "vs", conditions[1], "FPKMs", sep=" "),outer=TRUE, line = -2)
 
 dev.off()
 
-##Heatmap
-# Identify the genes (rows) below p-value 0.05
-sigpi = which(results_genes[,"pval"]<0.05)
+## Heatmap
+# Identify the genes (rows) below q-value 0.05
+sigpi = which(results_genes[,"qval"]<0.05)
 
 # Extract p-significant genes in a separate object
 sigp = results_genes[sigpi,]
@@ -304,7 +308,6 @@ sig_gene_expression=sig_gene_expression[,-c(7:8)]
 # for pheatmap function, column names and row names of data and pdata mush be identical# change the row names
 phenotype_table= data.frame(id=pheno_data$id, group=pheno_data$type)
 
-
 rownames(phenotype_table)=phenotype_table[,1]
 
 # remove the id column
@@ -315,15 +318,15 @@ colnames(sig_gene_expression)=row.names(phenotype_table)
 
 # draw heatmap
 if(image.color){
-  pheatmap(as.matrix(sig_gene_expression), kmeans_k = NA, scale = "row", clustering_distance_rows = "correlation", clustering_method = "complete",annotation_col = phenotype_table , main="Significant genes", filename=paste(workingDirectory, 'heatmap.', image.format, sep=""),) 
+  pheatmap(as.matrix(sig_gene_expression), kmeans_k = NA, scale = "row", clustering_distance_rows = "correlation", clustering_method = "complete",annotation_col = phenotype_table , main="Significant genes", filename=paste(workingDirectory, 'heatmap.', image.format, sep=""), width=image.width*0.0033333333333333335, height=image.height*0.0033333333333333335, fontsize=image.width*10/3100, treeheight_row=image.width*50/3100, treeheight_col=image.width*50/3100) 
 }else{
    group        <- c("gray20", "gray80")
    names(group) <- c(toString(conditions[1]), toString(conditions[2]))
    anno_colors <- list(group = group)
-  pheatmap(as.matrix(sig_gene_expression), kmeans_k = NA, scale = "row", clustering_distance_rows = "correlation", clustering_method = "complete",annotation_col = phenotype_table , main="Significant genes", filename=paste(workingDirectory, 'heatmap.', image.format, sep=""), color=gray.colors(20, start = 0.9, end = 0.2, gamma = 2.2, alpha = NULL), annotation_colors = anno_colors)
+   pheatmap(as.matrix(sig_gene_expression), kmeans_k = NA, scale = "row", clustering_distance_rows = "correlation", clustering_method = "complete",annotation_col = phenotype_table , main="Significant genes", filename=paste(workingDirectory, 'heatmap.', image.format, sep=""), width=image.width*0.0033333333333333335, height=image.height*0.0033333333333333335, fontsize=image.width*10/3100, treeheight_row=image.width*50/3100, treeheight_col=image.width*50/3100, color=gray.colors(20, start = 0.9, end = 0.2, gamma = 2.2, alpha = NULL), annotation_colors = anno_colors)
 }
 
-##PCA plot samples
+## PCA plot samples
 # transpose the data and compute principal components
 pca_data=prcomp(t(sig_gene_expression))
 
@@ -334,11 +337,11 @@ pca_data_perc=round(100*pca_data$sdev^2/sum(pca_data$sdev^2),1)
 df_pca_data = data.frame(PC1 = pca_data$x[,1], PC2 = pca_data$x[,2], sample = colnames(sig_gene_expression), condition = pheno_data$type)
 
 if(image.format == "jpeg") {
-	jpeg(paste(workingDirectory, 'pca_plot.jpeg',sep=""), width = image.width, height = image.height)
+	jpeg(paste(workingDirectory, 'pca.jpeg',sep=""), width = image.width, height = image.height)
 } else if(image.format == "tiff") {
-	tiff(paste(workingDirectory, 'pca_plot.tiff',sep=""), width = image.width, height = image.height)
+	tiff(paste(workingDirectory, 'pca.tiff',sep=""), width = image.width, height = image.height)
 } else if(image.format == "png") {
-	png(paste(workingDirectory, 'pca_plot.png',sep=""), width = image.width, height = image.height)
+	png(paste(workingDirectory, 'pca.png',sep=""), width = image.width, height = image.height)
 } 
 
 if(image.color){
